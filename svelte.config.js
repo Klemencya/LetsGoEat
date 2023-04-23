c91006edb14c7e9309199ee44978c13cc11c4d56
ritasidorska <m.sidorskaya@innopolis.university>
2023-04-23 13:49:13 +0300

    Изменили адаптер

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-cloudflare'
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 const dev = process.argv.includes('dev');
 
@@ -13,6 +13,9 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
+		prerender: {
+			default: true
+		},
 		paths: {
 			base: dev ? '' : '/LetsGoEat',
 		},
