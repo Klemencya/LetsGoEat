cd05c98ab4aff228ae9eb2dfd87cbe9137e0b354
IrSokolova <i.sokolova@innopolis.university>
2023-04-25 12:19:46 +0300

    GH actions

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,42 +1,42 @@
 <script lang="ts">
-    import { base } from "$app/paths";
+  import { base } from "$app/paths";
 </script>
 
 <nav>
-    <a href="{base}/" style="padding-left: 30%">Home</a>
-<!--    <a href="{base}/account" style="float:right; padding-right: 30%;"-->
-<!--        >My account</a-->
-<!--    >-->
+  <a href="{base}/" style="padding-left: 30%">Home</a>
+  <!--    <a href="{base}/account" style="float:right; padding-right: 30%;"-->
+  <!--        >My account</a-->
+  <!--    >-->
 </nav>
 
 <slot />
 
 <style>
-    @import url("https://fonts.googleapis.com/css2?family=Shrikhand&family=Ubuntu:wght@400;700&display=swap");
+  @import url("https://fonts.googleapis.com/css2?family=Shrikhand&family=Ubuntu:wght@400;700&display=swap");
 
-    nav {
-        padding-top: 1%;
-        padding-bottom: 1%;
-        background-color: #85d2ac;
-        font-size: 20px;
-        font-family: "Ubuntu", sans-serif;
-        font-weight: 700;
-        position: absolute;
-        top: 0px;
-        left: 0px;
-        width: 100%;
-        font-size: 24px;
-    }
+  nav {
+    padding-top: 1%;
+    padding-bottom: 1%;
+    background-color: #85d2ac;
+    font-size: 20px;
+    font-family: "Ubuntu", sans-serif;
+    font-weight: 700;
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    width: 100%;
+    font-size: 24px;
+  }
 
-    nav a {
-        color: #ffffff;
-        text-decoration: none;
-        display: inline-block;
-        -webkit-transition: all 0.2s ease-in-out;
-        transition: all 0.2s ease-in-out;
-    }
+  nav a {
+    color: #ffffff;
+    text-decoration: none;
+    display: inline-block;
+    -webkit-transition: all 0.2s ease-in-out;
+    transition: all 0.2s ease-in-out;
+  }
 
-    nav a:hover {
-        color: #9e4eca;
-    }
+  nav a:hover {
+    color: #9e4eca;
+  }
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,80 +1,83 @@
 <script lang="ts">
-    import { base } from "$app/paths";
+  import { base } from "$app/paths";
 </script>
 
 <div class="form">
-    <h1>Let's Go Eat</h1>
-    <div id="info-block">
-        <p>This app has been specially designed for people who can't find company to eat.</p>
-        <p>Just choose your company and eat</p>
-    </div>
-    <nav>
-        <a href="{base}/login"><button>Log in</button></a>
-        <a href="{base}/registration"><button>Registration</button></a>
-    </nav>
+  <h1>Let's Go Eat</h1>
+  <div id="info-block">
+    <p>
+      This app has been specially designed for people who can't find company to
+      eat.
+    </p>
+    <p>Just choose your company and eat</p>
+  </div>
+  <nav>
+    <a href="{base}/login"><button>Log in</button></a>
+    <a href="{base}/registration"><button>Registration</button></a>
+  </nav>
 </div>
 
 <style>
-    @import url("https://fonts.googleapis.com/css2?family=Shrikhand&family=Ubuntu:wght@400;700&display=swap");
-    .form {
-        text-align: center;
-        margin-right: 30%;
-        margin-left: 30%;
-        margin-top: 10%;
-    }
+  @import url("https://fonts.googleapis.com/css2?family=Shrikhand&family=Ubuntu:wght@400;700&display=swap");
+  .form {
+    text-align: center;
+    margin-right: 30%;
+    margin-left: 30%;
+    margin-top: 10%;
+  }
 
-    div h1 {
-        font-size: 64px;
-        font-family: "Shrikhand", cursive;
-        color: #9e4eca;
-    }
+  div h1 {
+    font-size: 64px;
+    font-family: "Shrikhand", cursive;
+    color: #9e4eca;
+  }
 
-    div nav {
-        display: flex;
-        flex-direction: column;
-    }
+  div nav {
+    display: flex;
+    flex-direction: column;
+  }
 
-    nav button {
-        margin-top: 10px;
-        width: 50%;
-    }
+  nav button {
+    margin-top: 10px;
+    width: 50%;
+  }
 
-    button {
-        width: 200px;
-        font-size: 20px;
-        font-family: "Ubuntu", sans-serif;
-        font-weight: 700;
-        color: #fff;
-        cursor: pointer;
-        margin: 20px;
-        height: 55px;
-        text-align: center;
-        border: none;
-        background-size: 300% 100%;
+  button {
+    width: 200px;
+    font-size: 20px;
+    font-family: "Ubuntu", sans-serif;
+    font-weight: 700;
+    color: #fff;
+    cursor: pointer;
+    margin: 20px;
+    height: 55px;
+    text-align: center;
+    border: none;
+    background-size: 300% 100%;
 
-        border-radius: 50px;
-        moz-transition: all 0.4s ease-in-out;
-        -o-transition: all 0.4s ease-in-out;
-        -webkit-transition: all 0.4s ease-in-out;
-        transition: all 0.4s ease-in-out;
-        background-image: linear-gradient(
-            to right,
-            #9e4eca,
-            #7584b8,
-            #8abcc3,
-            #85d2ac
-        );
-        box-shadow: 0 4px 15px 0 rgba(158, 184, 198, 0.75);
-    }
+    border-radius: 50px;
+    moz-transition: all 0.4s ease-in-out;
+    -o-transition: all 0.4s ease-in-out;
+    -webkit-transition: all 0.4s ease-in-out;
+    transition: all 0.4s ease-in-out;
+    background-image: linear-gradient(
+      to right,
+      #9e4eca,
+      #7584b8,
+      #8abcc3,
+      #85d2ac
+    );
+    box-shadow: 0 4px 15px 0 rgba(158, 184, 198, 0.75);
+  }
 
-    button:hover {
-        background-position: 100% 0;
-        moz-transition: all 0.4s ease-in-out;
-        -o-transition: all 0.4s ease-in-out;
-        -webkit-transition: all 0.4s ease-in-out;
-        transition: all 0.4s ease-in-out;
-    }
-    #info-block {
-        padding-bottom: 50px;
-    }
+  button:hover {
+    background-position: 100% 0;
+    moz-transition: all 0.4s ease-in-out;
+    -o-transition: all 0.4s ease-in-out;
+    -webkit-transition: all 0.4s ease-in-out;
+    transition: all 0.4s ease-in-out;
+  }
+  #info-block {
+    padding-bottom: 50px;
+  }
 </style>
